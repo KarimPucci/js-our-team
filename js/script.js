b9f3d48c0d031d12ac3e950a6d9da8bf71d93e74
Karim <kizpucci@gmail.com>
2022-10-25 20:40:50 +0200

    milestone1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
-console.log("helloooo");
+const row = document.querySelector(".row");
+const titolo = document.getElementById("titolo");
+
+titolo.innerHTML = "OUR TEAM";
+
+const cards = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        immagine: "img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        immagine: "img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        immagine: "img/walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        immagine: "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome: "Scoot Estrada",
+        ruolo: "Developer",
+        immagine: "img/scott-estrada-developer.jpg",
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        immagine: "img/barbara-ramos-graphic-designer.jpg",
+    }
+];
+console.log(cards);
+
+for(let team of cards){
+    console.log(team);
+}
